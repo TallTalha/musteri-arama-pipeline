ccdeff87e4a517b0e51ba96c26c8f2b1b0c797f2
TallTalha <talhaburakaydin.official@gmail.com>
2025-07-09 11:03:50 +0300

    feat: Batch analizi için fonksiyonlar eklendi ve mongodb bağlantısı ve yazma fonksiyonu eklendi

--- spark-consumer/batch_consumer.py
+++ spark-consumer/batch_consumer.py
@@ -1,19 +1,24 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, from_json
-from pyspark.sql.types import StructType, StructField, StringType, LongType
+from pyspark.sql import SparkSession # type: ignore
+from pyspark.sql.functions import col, from_json, desc # type: ignore
+from pyspark.sql.types import StructType, StructField, StringType, LongType #type: ignore
 import os
 
 def main():
+
+    kafka_mongo_connector_packages = "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1,org.mongodb.spark:mongo-spark-connector_2.12:10.3.0"
+    mongo_con_uri = "mongodb://localhost:27017/eticaret.analizler"
     # SparkSession oluştur
     spark = ( SparkSession.builder
-        .appName("KafkaBatchConsumer")  
+        .appName("KafkaToMongoBatchJob")  
         .master("local[*]") 
-        .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1")
+        .config("spark.jars.packages", kafka_mongo_connector_packages)
+        .config("spark.mongodb.write.connection.uri",mongo_con_uri)
         .getOrCreate()
     ) 
     
     spark.sparkContext.setLogLevel("WARN")  # Log seviyesini WARN olarak ayarla
-
+    print("Spark session oluşuturuldu.")
+    
     kafka_topic = "search-logs"
     kafka_server = os.environ.get("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
     print(f"Kafka'ya bağlanılıyor: {kafka_server}")
@@ -39,6 +44,8 @@ def main():
     parsed_df = kafka_df.select(
     from_json(col("value").cast("string"), schema).alias("data")  # JSON string'i uygun schema ile ayrıştır
     ).select("data.*")  # Sadece ayrıştırılmış veriyi seç
+    parsed_df.cache()
+    print("Veri kafkadan okundu ve ayrıştırıldı.")
 
     record_count = parsed_df.count()  # Toplam kayıt sayısını al
     print(f"Toplam kayıt sayısı: {record_count}")
@@ -47,6 +54,46 @@ def main():
         print("Ayrıştırılmış verinin ilk 10 kaydı:")
         parsed_df.show(10, truncate=False)
 
+    # En Çok Arama Yapan 10 Şehir
+    top_10_cities = ( parsed_df
+        .groupBy("region")
+        .count()
+        .orderBy(col("count").desc())
+        .limit(10)
+    )
+
+    # En çok Arama Yapılan 10 Ürün
+    top_10_searches = ( parsed_df
+        .groupBy("search")
+        .count()
+        .orderBy(col("count").desc())
+        .limit(10)
+    )
+
+    # Lazy Evulation Tetikleyicisi
+    # Spark show/write gibi bir aksiyon görene kadar işlemleri gerçekten çalıştırmaz.
+    print("En çok arama yapılan 10 şehir:")
+    top_10_cities.show(truncate=False)
+    print("En çok araması yapılan 10 ürün:")
+    top_10_searches.show(truncate=False)
+    
+    
+    print("Veriler MongoDB'ye yazılıyor...")
+    (
+        top_10_cities
+        .format("mongodb")
+        .mode("overwrite")
+        .option("collection","top_10_sehirler")
+        .save()
+    )
+    (
+        top_10_searches
+        .format("mongodb")
+        .mode("overwrite")
+        .option("collection","top_10_aramalar")
+        .save()
+    )
+    
     spark.stop()  # Spark oturumunu kapat
     print("Spark oturumu kapatıldı.")
 
